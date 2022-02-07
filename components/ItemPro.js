efb3e49120a09476631a8b44c9ee728b2b3b31d9
Neto Pompeu <neto1@hotmail.fr>
2022-02-07 15:29:43 -0300

    modif detail mobile 375px

--- components/ItemPro.js
+++ components/ItemPro.js
@@ -5,17 +5,19 @@ export const ItemPro = [
     descrip: "Web / Landing Page en Next.js & conseil ",
     image: "/button rond.svg",
     alt: "Petit bouton pour voir le projet fini",
-    imagePro: "/Projet/website rps agriculture guyane.png",
-    altPro: "$",
+    imagePro: "/Projet/WEBSITE SOARES OUEST CONSTRUCTION SLM.png",
+    altPro:
+      "Image du site web développé pour la société guyanaise Soares Ouest Construction sur Saint Laurent Du Maroni",
   },
   {
     company: "RPS Agriculture",
-    date: "Janvier 2022",
+    date: "Fevrier 2022",
     descrip: "Web / Landing Page en Next.js & conseil ",
     image: "/button rond.svg",
     alt: "Petit bouton pour voir le projet fini",
     imagePro: "/Projet/website rps agriculture guyane.png",
-    altPro: "$",
+    altPro:
+      "Image du site web pour l'agricultrice RPS sur Saint Laurent du maroni",
   },
   {
     company: "SAMBO PROMO PUBLICITE",
--- components/ItemSkill.js
+++ components/ItemSkill.js
@@ -1,32 +1,32 @@
 export const ItemSkill = [
   {
     title: "User Experience Design",
-    image: "/skill/img skill.svg",
-    alt: "Petit bouton pour voir le service proposé",
+    image: "/skill/ICON USER EXPERIENCE.svg",
+    alt: "icon user experience design par primo frances sur saint laurent du maroni",
   },
   {
     title: "Photo / Vidéo",
-    image: "/skill/img skill.svg",
-    alt: "Petit bouton pour voir le service proposé",
+    image: "/skill/ICON MEDIA.svg",
+    alt: "icon represente photo & video ",
   },
   {
     title: "Code development",
-    image: "/skill/img skill.svg",
-    alt: "Petit bouton pour voir le service proposé",
+    image: "/skill/ICON CODE.svg",
+    alt: "icon developpement web ",
   },
   {
     title: "Social Media",
-    image: "/skill/img skill.svg",
-    alt: "Petit bouton pour voir le service proposé",
+    image: "/skill/ICON SOCIAL MEDIA.svg",
+    alt: "icon social media skill",
   },
   {
     title: "SEO / SEA",
-    image: "/skill/img skill.svg",
-    alt: "Petit bouton pour voir le service proposé",
+    image: "/skill/ICON SEO SEA.svg",
+    alt: "icon represente SEO & SEA skill",
   },
   {
     title: "Webmaster",
-    image: "/skill/img skill.svg",
-    alt: "Petit bouton pour voir le service proposé",
+    image: "/skill/ICON WEBMASTER.svg",
+    alt: "icon webmaster pour le site primofrances sur slm",
   },
 ];
