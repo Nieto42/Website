cf4e73c8fa2a01df3d9b235f0ca3dd26464f0e91
Neto Pompeu <neto1@hotmail.fr>
2022-03-14 19:31:00 -0300

    add gtm or not

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head></Head>
+
       <body>
         <Main />
         <NextScript />
